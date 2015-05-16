47810b3b8c3a588be5ece37dda3a4b97867c288b
한기태 <@192.168.30.1>
2015-05-16 13:41:02 +0900

    과제 제출

--- ProjectTeam/src/kr/ac/shinhan/csp/Logout.java
+++ ProjectTeam/src/kr/ac/shinhan/csp/Logout.java
@@ -11,33 +11,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @SuppressWarnings("serial")
 public class Logout extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
 		HttpSession session = req.getSession(false);
-		session.invalidate();
-		
+
 		PersistenceManager pm = MemberManager.dogetManager();
 		Query qry = pm.newQuery(UserLoginToken.class);
-		List<UserLoginToken> userLoginToken = (List<UserLoginToken>) qry.execute();
-		
-		boolean success = false;
-		
-		Cookie[] cookieList = req.getCookies();
+		List<UserLoginToken> userLoginToken = (List<UserLoginToken>) qry
+				.execute();
 
-		for (Cookie c : cookieList)
-		{			
-			if (c.getName().equals("token_id"))
-			{
+		boolean success = false;
+
+		Cookie[] cookies = req.getCookies();
+
+		for (Cookie c : cookies) {
+			if (c.getName().equals("token_id")) {
 				String token = c.getValue();
-				for(UserLoginToken ult : userLoginToken)
-				{
-					if (ult.getToken().equals(token))
-					{
-						//토큰이 같을 때 해당항목 지움 그리고 트루 줌 
+				for (UserLoginToken ult : userLoginToken) {
+					if (ult.getToken().equals(token)) {
 						pm.deletePersistent(ult);
 						success = true;
 					}
@@ -46,51 +40,52 @@ public class Logout extends HttpServlet {
 				c.setMaxAge(0);
 				resp.addCookie(c);
 			}
-			
+
 		}
-		if (success)
-		{
-			resp.sendRedirect("/login.html");
-		}
-		/*req.setCharacterEncoding("EUC-KR");
-		resp.setContentType("text/plain;charset=EUC-KR");
+		if (cookies != null) {
 
-		Long key = null;
-		HttpSession s = req.getSession(false);
+			for (int i = 0; i < cookies.length; i++) {
 
-		if (s == null) {
-			resp.getWriter().println("");
-		}
-		
-		
-		else 
-		{
-			key = (Long) s.getAttribute("check_key");
-			if (key != null) 
-			{
+				if (cookies[i].getName().equals("token_id")) {
 
-				Cookie[] cookieList = req.getCookies();
+					cookies[i].setMaxAge(0);
 
-				for (Cookie c : cookieList) 
-				{
-					if (c.getName().equals("token_id")) 
-					{
-						c.setValue(null);
-						c.setMaxAge(0);
-						resp.addCookie(c);
-						
-					}
+					resp.addCookie(cookies[i]);
 				}
-
-				PersistenceManager pm = MemberManager.dogetManager();
-				UserLoginToken usertok = pm.getObjectById(UserLoginToken.class, key);
-				pm.deletePersistent(usertok);
-
-				session.setMaxInactiveInterval(0);
-				session.invalidate();
-
 			}
-
-		}*/
+			
+			session.setMaxInactiveInterval(0);
+			session.invalidate();
+				resp.sendRedirect("/login.html");
+			/*
+			 * req.setCharacterEncoding("EUC-KR");
+			 * resp.setContentType("text/plain;charset=EUC-KR");
+			 * 
+			 * Long key = null; HttpSession s = req.getSession(false);
+			 * 
+			 * if (s == null) { resp.getWriter().println(""); }
+			 * 
+			 * 
+			 * else { key = (Long) s.getAttribute("check_key"); if (key != null)
+			 * {
+			 * 
+			 * Cookie[] cookieList = req.getCookies();
+			 * 
+			 * for (Cookie c : cookieList) { if (c.getName().equals("token_id"))
+			 * { c.setValue(null); c.setMaxAge(0); resp.addCookie(c);
+			 * 
+			 * } }
+			 * 
+			 * PersistenceManager pm = MemberManager.dogetManager();
+			 * UserLoginToken usertok = pm.getObjectById(UserLoginToken.class,
+			 * key); pm.deletePersistent(usertok);
+			 * 
+			 * session.setMaxInactiveInterval(0); session.invalidate();
+			 * 
+			 * }
+			 * 
+			 * }
+			 */
+		}
 	}
 }
